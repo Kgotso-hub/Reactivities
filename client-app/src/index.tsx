312632d58b1361683b1bad39f6cd41797f1b08f6
Kgotsofalang Maganya <kmaganya@singular.co.za>
2023-01-25 11:59:35 +0200

    End of Section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
+import 'react-calendar/dist/Calendar.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
